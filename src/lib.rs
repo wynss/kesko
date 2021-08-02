9723cc6d97d9a46acf6019341d64f79cddc341b0
wynss <toniaxelsson@gmail.com>
2021-08-02 16:17:38 +0200

    start bevy from python

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use pyo3::prelude::*;
+use bevy::prelude::*;
+
+
+fn hello_world() {
+    println!("Hello from bevy!")
+}
+
+#[pyfunction]
+fn run_bevy() -> PyResult<()> {
+    App::build()
+        .add_startup_system(hello_world.system())
+        .run();
+
+    Ok(())
+}
+
+#[pymodule]
+fn nora(_py: Python, m: &PyModule) -> PyResult<()> {
+
+    m.add_function(wrap_pyfunction!(run_bevy, m)?)?;
+
+    Ok(())
 }
