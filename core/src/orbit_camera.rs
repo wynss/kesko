260bfd48b0b6a4c2c97b81a44a0bcea422ed1fc6
wynss <toniaxelsson@icloud.com>
2022-04-29 17:31:17 +0200

    Refactor pan orbit camera (#6)
    refactor pan orbit camera to make it a seperate crate in the future
    
    also made it possible to hide/show menus
    
    Co-authored-by: wynss <toniaxelsson@gmail.com>

---
+++ core/src/orbit_camera.rs
@@ -0,0 +1,152 @@
+use bevy::input::mouse::{MouseMotion, MouseWheel};
+use bevy::prelude::*;
+
+
+pub struct PanOrbitCameraPlugin;
+
+impl Plugin for PanOrbitCameraPlugin {
+    fn build(&self, app: &mut App) {
+
+        app.add_system(handle_camera_mouse_events)
+            .add_system(handle_camera_events)
+            .add_event::<PanOrbitCameraEvents>();
+    }
+}
+
+#[derive(Debug)]
+enum PanOrbitCameraEvents {
+    Orbit(Vec2),
+    Pan(Vec2),
+    Zoom(f32)
+}
+
+#[derive(Component)]
+pub struct PanOrbitCamera {
+    pub center: Vec3,
+    pub distance: f32,
+    pub orbit_sensitivity: f32,
+    pub pan_sensitivity: f32,
+    pub zoom_sensitivity: f32,
+    orbit_button: MouseButton,
+    pan_button: MouseButton
+}
+
+impl Default for PanOrbitCamera {
+    fn default() -> Self {
+        PanOrbitCamera {
+            center: Vec3::ZERO,
+            distance: 5.0,
+            orbit_sensitivity: 1.0,
+            pan_sensitivity: 40.0,
+            zoom_sensitivity: 0.05,
+            orbit_button: MouseButton::Right,
+            pan_button: MouseButton::Middle
+        }
+    }
+}
+
+pub fn spawn_camera(mut commands: Commands) {
+
+    let camera_pos = Vec3::new(-2.0, 2.5, 5.0);
+    let distance = camera_pos.length();
+
+    let camera_transform = Transform::from_translation(camera_pos)
+        .looking_at(Vec3::ZERO, Vec3::Y);
+
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: camera_transform,
+        ..Default::default()
+    }).insert(PanOrbitCamera {
+        distance,
+        ..Default::default()
+    });
+}
+
+fn handle_camera_mouse_events(
+    mut events: EventWriter<PanOrbitCameraEvents>,
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut mouse_scroll_events: EventReader<MouseWheel>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    query: Query<&PanOrbitCamera>
+) {
+
+    let mut motion_delta = Vec2::ZERO;
+    let mut scroll_delta = 0.0;
+
+    for event in mouse_motion_events.iter() {
+        motion_delta += event.delta;
+    }
+
+    for event in mouse_scroll_events.iter() {
+        scroll_delta += event.y;
+    }
+
+    if motion_delta.length() > 0.0 {
+        for camera in query.iter() {
+            if mouse_button_input.pressed(camera.orbit_button) {
+                events.send(PanOrbitCameraEvents::Orbit(motion_delta));
+            }
+            else if mouse_button_input.pressed(camera.pan_button) {
+                events.send(PanOrbitCameraEvents::Pan(motion_delta));
+            }
+        }
+    }
+
+    if scroll_delta.abs() > 0.0 {
+        for _ in query.iter() {
+            events.send(PanOrbitCameraEvents::Zoom(scroll_delta));
+        }
+    }
+}
+
+fn handle_camera_events(
+    time: Res<Time>,
+    windows: Res<Windows>,
+    mut camera_events: EventReader<PanOrbitCameraEvents>,
+    mut query: Query<(&mut PanOrbitCamera, &mut Transform, &PerspectiveProjection)>
+) {
+
+    let window = windows.get_primary().unwrap();
+    let window_size = Vec2::new(window.width(), window.height());
+
+    for (mut camera, mut transform, projection) in query.iter_mut() {
+        for event in camera_events.iter() {
+            match event {
+                PanOrbitCameraEvents::Orbit(mouse_move) => {
+
+                    // get rotation that should be performed around x and y axes
+                    let rot_around_y = mouse_move.x * camera.orbit_sensitivity * time.delta_seconds();
+                    let rot_around_x = mouse_move.y * camera.orbit_sensitivity * time.delta_seconds();
+                    transform.rotation = {
+                        Quat::from_rotation_y(-rot_around_y)
+                            * transform.rotation
+                            * Quat::from_rotation_x(-rot_around_x)
+                    };
+                }
+                PanOrbitCameraEvents::Pan(mut mouse_move) => {
+
+                    // make panning distance independent of resolution and FOV
+                    mouse_move *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window_size;
+
+                    // translate by local camera axes
+                    let pan_right = transform.rotation * Vec3::X * mouse_move.x;
+                    let pan_up = transform.rotation * Vec3::Y * mouse_move.y;
+
+                    let translation = (pan_up - pan_right)
+                        * camera.distance  // make panning proportional to distance away from focus point
+                        * time.delta_seconds()
+                        * camera.pan_sensitivity;
+
+                    camera.center += translation;
+                }
+                PanOrbitCameraEvents::Zoom(scroll_move) => {
+                    camera.distance -= camera.distance * camera.zoom_sensitivity * (*scroll_move);
+                    camera.distance = f32::max(camera.distance, 0.05);
+                }
+            }
+
+            // translate so camera always is on the sphere centered around camera.center with radius camera.distance
+            transform.translation = camera.center + (transform.rotation * Vec3::Z) * camera.distance;
+        }
+    }
+}
--- core/src/plugins/menu.rs
+++ core/src/plugins/menu.rs
@@ -6,12 +6,35 @@ use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 
 #[derive(Default)]
+pub struct OccupiedScreenSpaceByMenus {
+    pub left: f32,
+    pub top: f32,
+    pub right: f32,
+    pub bottom: f32
+}
+
+struct MenusContext {
+    should_hide: bool
+}
+
+impl Default for MenusContext {
+    fn default() -> Self {
+        MenusContext {
+            should_hide: true
+        }
+    }
+}
+
+
+#[derive(Default)]
 pub(crate) struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(EguiPlugin)
-            .add_system(side_menus);
+            .add_system(update_menus)
+            .init_resource::<MenusContext>()
+            .init_resource::<OccupiedScreenSpaceByMenus>();
     }
 
     fn name(&self) -> &str {
@@ -19,49 +42,74 @@ impl Plugin for MenuPlugin {
     }
 }
 
-fn side_menus(
+fn update_menus(
+    mut occupied_screen_space_by_menus: ResMut<OccupiedScreenSpaceByMenus>,
+    mut menus_ctx: ResMut<MenusContext>,
     mut egui_ctx: ResMut<EguiContext>
 ) {
 
-    egui::TopBottomPanel::top("top_menu")
+    occupied_screen_space_by_menus.top = egui::TopBottomPanel::top("top_menu")
         .show(egui_ctx.ctx_mut(), |ui| {
             egui::menu::bar(ui, |ui| {
 
                 egui::widgets::global_dark_light_mode_switch(ui);
 
-                ui.menu_button("File", |_ui| {
+                ui.menu_button("View", |ui| {
+                    if ui.button("Show/Hide menus").clicked() {
+                        menus_ctx.should_hide = !menus_ctx.should_hide;
 
+                        if menus_ctx.should_hide {
+                            occupied_screen_space_by_menus.top = 0.0;
+                            occupied_screen_space_by_menus.right = 0.0;
+                            occupied_screen_space_by_menus.bottom = 0.0;
+                            occupied_screen_space_by_menus.left = 0.0;
+                        }
+
+                        ui.close_menu();
+                    }
                 });
             });
-        });
+        })
+        .response
+        .rect
+        .top();
 
-    egui::SidePanel::left("left_side_menu")
-        .resizable(true)
-        .default_width(200.0)
-        .max_width(400.0)
-        .show(egui_ctx.ctx_mut(), |ui| {
-            if ui.button("Load URDF").clicked() {
+    if !menus_ctx.should_hide {
 
-            }
+        occupied_screen_space_by_menus.left = egui::SidePanel::left("left_side_menu")
+            .resizable(true)
+            .default_width(200.0)
+            .max_width(400.0)
+            .show(egui_ctx.ctx_mut(), |ui| {
+                if ui.button("Load URDF").clicked() {}
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .left();
 
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        });
+        occupied_screen_space_by_menus.right = egui::SidePanel::right("right_side_menu")
+            .resizable(true)
+            .default_width(200.0)
+            .max_width(400.0)
+            .show(egui_ctx.ctx_mut(), |ui| {
+                ui.heading("Right Menu");
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .right();
 
-    egui::SidePanel::right("right_side_menu")
-        .resizable(true)
-        .default_width(200.0)
-        .max_width(400.0)
-        .show(egui_ctx.ctx_mut(), |ui| {
-            ui.heading("Right Menu");
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        });
-
-    egui::TopBottomPanel::bottom("bottom_menu")
-        .resizable(true)
-        .default_height(100.0)
-        .max_height(200.0)
-        .show(egui_ctx.ctx_mut(), |ui| {
-            ui.heading("Bottom Menu");
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        });
+        occupied_screen_space_by_menus.bottom = egui::TopBottomPanel::bottom("bottom_menu")
+            .resizable(true)
+            .default_height(100.0)
+            .max_height(200.0)
+            .show(egui_ctx.ctx_mut(), |ui| {
+                ui.heading("Bottom Menu");
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .bottom();
+    }
 }
