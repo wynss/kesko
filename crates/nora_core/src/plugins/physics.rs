9d1b2842a70a8b383422940329d5f3540b9e64a5
wynss <toniaxelsson@gmail.com>
2022-05-19 15:49:01 +0200

    remove heron dep (#20)

--- crates/nora_core/src/plugins/physics.rs
+++ crates/nora_core/src/plugins/physics.rs
@@ -1,8 +1,5 @@
-use bevy::{
-    app::{App, Plugin},
-    math::Vec3
-};
-use heron::{PhysicsPlugin, Gravity};
+use bevy::app::{App, Plugin};
+use nora_physics::PhysicsPlugin;
 
 
 #[derive(Default)]
@@ -10,7 +7,6 @@ pub(crate) struct DefaultPhysicsPlugin;
 
 impl Plugin for DefaultPhysicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(PhysicsPlugin::default())
-            .insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0)));
+        app.add_plugin(PhysicsPlugin::gravity());
     }
 }
