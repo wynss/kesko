bc6efd303a2e827c24979cfc2d30fe503e5f78d6
wynss <toniaxelsson@gmail.com>
2022-05-20 15:35:49 +0200

    add impulse component and sys

---
+++ crates/nora_physics/src/impulse.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+use rapier3d::prelude as rapier;
+use crate::rigid_body::RigidBodyHandle;
+use crate::conversions::IntoRapier;
+
+
+#[derive(Component, Default)]
+pub struct Impulse {
+    pub vec: Vec3
+}
+
+pub(crate) fn update_impulse(
+    mut rigid_bodies: ResMut<rapier::RigidBodySet>,
+    query: Query<(&RigidBodyHandle, &Impulse), With<Impulse>>
+) {
+    for (body_handle, impulse) in query.iter() {
+        if let Some(body) = rigid_bodies.get_mut(body_handle.0) {
+            if impulse.vec != Vec3::ZERO {
+                body.apply_impulse(impulse.vec.into_rapier(), true);
+            }
+        }
+    }
+}
+
