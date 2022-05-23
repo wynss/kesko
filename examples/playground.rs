e3802f37e4a960fd336ffff3bddec4a64626f07f
wynss <toniaxelsson@gmail.com>
2022-05-23 13:10:23 +0200

    initial bundle for physics objects

--- examples/playground.rs
+++ examples/playground.rs
@@ -1,6 +1,7 @@
 use std::f32::consts::PI;
 use bevy::prelude::*;
 use bevy::diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin};
+use nora_core::bundle::{PhysicBodyBundle, Shape};
 
 use nora_core::orbit_camera::{PanOrbitCameraPlugin, PanOrbitCamera};
 use nora_physics::{
@@ -35,16 +36,15 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
     // Spawn ground
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 200.0 })),
-        material: materials.add(Color::ALICE_BLUE.into()),
-        ..Default::default()
-    })
-        .insert(RigidBody::Fixed)
-        .insert(ColliderShape::Cuboid {x_half: 100.0, y_half: 0.0, z_half: 100.0})
-        .insert_bundle(InteractiveBundle::default());
+    commands.spawn_bundle(PhysicBodyBundle::from(
+        RigidBody::Fixed,
+        Shape::Plane {size: 200.0},
+        materials.add(Color::ALICE_BLUE.into()),
+        Transform::default(),
+        &mut meshes
+    ));
 
-    // Spawn sphere
+    // Spawn spheres
     let num_sphere = 5.0;
     let radius = 4.0;
     for i in 0..(num_sphere as i32) {
@@ -54,22 +54,13 @@ fn setup(
         let x = radius * (2.0 * PI * i_f / num_sphere).cos();
         let sphere_radius = 0.5 * (1.0 + i_f);
 
-        commands.spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere { radius: sphere_radius, subdivisions: 8})),
-            material: materials.add(Color::hex("66BB6A").unwrap().into()),
-            transform: Transform::from_xyz(x, 10.0, y),
-            ..Default::default()
-        })
-            .insert(RigidBody::Dynamic)
-            .insert(ColliderShape::Sphere { radius: sphere_radius })
-            .insert(ColliderPhysicalProperties {
-                restitution: 0.1*(i as f32),
-                ..Default::default()
-            })
-            .insert(Impulse::default())
-            .insert(GravityScale::default())
-            .insert_bundle(InteractiveBundle::default());
-
+        commands.spawn_bundle(PhysicBodyBundle::from(
+            RigidBody::Dynamic,
+            Shape::Sphere {radius: sphere_radius, subdivisions: 5},
+            materials.add(Color::hex("66BB6A").unwrap().into()),
+            Transform::from_xyz(x, 10.0, y),
+            &mut meshes
+        )).insert_bundle(InteractiveBundle::default());
     }
 
     // camera
