2efb496602ceb6fafa143c2077274227cae8f45d
wynss <toniaxelsson@gmail.com>
2022-09-09 22:07:08 +0200

    make sure we read all events and hide correctly
    sometimes two events arrive at the sametime so we need to read all.
    also make sure we are hiden when the correct body is deselected.

--- crates/nora_core/src/ui/multibody_component.rs
+++ crates/nora_core/src/ui/multibody_component.rs
@@ -9,9 +9,9 @@ use nora_object_interaction::event::SelectEvent;
 /// UI Component that handles showing multibody information and also controlling them
 #[derive(Component, Default)]
 pub(crate) struct MultibodyMenuComponent {
-    open: bool,
-    body_name: String,
-    root_entity: Option<Entity>
+    ui_open: bool,
+    multibody_name: String,
+    multibody_root: Option<Entity>
 }
 
 impl MultibodyMenuComponent {
@@ -27,41 +27,39 @@ impl MultibodyMenuComponent {
         // get a reference to 'self'
         let mut comp = comp.get_single_mut().expect("We should have a component");
 
-        // read a possible event that a multibody has been selected/deselected
-        if !multibody_select_event_reader.is_empty() {
-            if let Some(event) = multibody_select_event_reader.iter().last() {
-                match event {
-                    MultibodySelectionEvent::Selected(root_entity) => {
-                        comp.root_entity = Some(*root_entity);
-                        comp.open = true;
-                        if let Ok(root) = body_query.get(*root_entity) {
-                            comp.body_name = root.name.clone();
-                        }
-                    },
-                    MultibodySelectionEvent::Deselected(entity) => {
-                        if Some(*entity) == comp.root_entity {
-                            comp.root_entity = None;
-                            comp.open = false;
-                        }
+        // read a possible events that a multibody has been selected/deselected
+        for event in multibody_select_event_reader.iter() {
+            match event {
+                MultibodySelectionEvent::Selected(root_entity) => {
+                    comp.multibody_root = Some(*root_entity);
+                    comp.ui_open = true;
+                    if let Ok(root) = body_query.get(*root_entity) {
+                        comp.multibody_name = root.name.clone();
+                    }
+                },
+                MultibodySelectionEvent::Deselected(entity) => {
+                    // make sure we are only closing when the correct entity is deselected
+                    if Some(*entity) == comp.multibody_root {
+                        comp.multibody_root = None;
+                        comp.ui_open = false;
                     }
                 }
             }
         }
-        
         // run ui logic
         comp.show(egui_context.ctx_mut(), &mut select_event_writer);
     }
 
     fn show(&mut self, ctx: &egui::Context, select_event_writer: &mut EventWriter<SelectEvent>) {
         egui::Window::new("Multibody View")
-            .open(&mut self.open)
+            .open(&mut self.ui_open)
             .resizable(true)
             .title_bar(false)
             .show(ctx, |ui| {
                 ui.vertical(|ui| {
-                    ui.heading(&self.body_name);
+                    ui.heading(&self.multibody_name);
                     if ui.button("Close").clicked() {
-                        if let Some(entity) = self.root_entity {
+                        if let Some(entity) = self.multibody_root {
                             select_event_writer.send(SelectEvent::Deselect(entity))
                         }
                     }
