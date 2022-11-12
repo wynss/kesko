3950763dec23e4a00d91e6c262e3ce53038f2ae5
wynss <toniaxelsson@gmail.com>
2022-11-12 15:15:27 +0100

    add python versions and change upload dir

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 for PYBIN in /opt/python/cp3[7891011]*/bin; do
     "${PYBIN}/pip" install maturin
-    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
+    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release --manylinux 2014
 done
 
 for wheel in target/wheels/*.whl; do
