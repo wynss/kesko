4e0204a448971a83ecc36562a78a0d31381eaab5
Polgár András <azazdeaz@gmail.com>
2023-07-07 15:24:02 +0200

    Fix absolute path

--- pykesko/build.rs
+++ pykesko/build.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 fn main() {
     flatc_rust::run(flatc_rust::Args {
         lang: "python",
-        inputs: &[Path::new("/home/azazdeaz/repos/kesko/kesko/crates/kesko_urdf/definitions/urdf.fbs")],
+        inputs: &[Path::new("../kesko/crates/kesko_urdf/definitions/urdf.fbs")],
         out_dir: Path::new("./messages"),
         ..Default::default()
     })
